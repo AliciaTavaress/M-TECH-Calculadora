1395ceee4ac3f6962339ba9846e2190f8f64b7eb
Alicia <aliciats019@gmail.com>
2024-12-04 17:48:39 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,17 @@
+function appendToDisplay(value){
+    document.getElementById("tela").value+=value;
+}
+
+function clearDisplay(){
+    document.getElementById("tela").value="";
+}
+
+function calculate() {
+    try {
+        const result = eval(document.getElementById("tela").value);
+        document.getElementById("tela").value = result;
+    } catch(erro){
+        alert("Calculo errado.")
+    }
+    
+}
